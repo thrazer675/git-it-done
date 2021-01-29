7a051eac304d4276f1f6c9fa7ad5a18e60249ae8
thrazer675 <thrazer675@aol.com>
2021-01-28 23:12:41 -0500

    add error messages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,19 @@
+var limitWarningEl = document.querySelector("#limit-warning");
+
 var issueContainerEl = document.querySelector("#issues-container");
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,6 +22,10 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!");
